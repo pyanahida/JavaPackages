23f4deb727104eaa581f4707bff68ab27a88cfbe
pyanahida <dqybizib@126.com>
2026-09-05 17:36:13 +0800

    Refactor UnusefulFunc methods and change EPSILON
    Refactor methods and update EPSILON variable to be non-final.

--- UnusefulFunc.java
+++ UnusefulFunc.java
@@ -1,21 +1,42 @@
 package com.pyanahida.functions.unuseful;
 
 public class UnusefulFunc {
-    static private final double EPSILON = 1E-9D;
+    static private double EPSILON = 1E-9D;
     
-    static public double SqrtPiPow(double x) {
-        // \frac{\frac{47x}{72\sqrt{\frac{\pi}{8^x}}}}{32x}
+    public static double SqrtPiExp(double x) {
+        // y=\frac{\frac{47x}{72\sqrt{\frac{\pi}{8^x}}}}{32x}
         if ((-EPSILON < x) && (x < EPSILON)) return Double.NaN;
         double a = 47D * x;
         double b = 72D * Math.sqrt(Math.PI / Math.pow(8D, x));
         double c = 32D * x;
         return (a / b) / c;
     }
-
-    static public double CosDenominatorRationalFunc(double x) {
-        // \frac{x}{1+\cos x}
+    
+    public static double SqrtPiExp(double x) {
+        // y=\frac{x}{1+\cos x}
         double c = Math.cos(x);
         if ((-EPSILON < (c+1)) && ((c+1) < EPSILON)) return Double.NaN;
         return x / (1D + c);
+    }
+    
+    public static boolean XyFixedEq(int x, int y) {
+        // y=xy
+        return (x == 1) || (y == 0);
+    }
+    
+    public static double ExpSinProductFunc(double) {
+        // y=e^{x}\sin(100xe)
+        return Math.exp(x)*Math.sin(100D*x*Math.E);
+    }
+    
+    public static double SinAsinForPow(double x) {
+        // y=\sin^{2}(\arcsin(x^9))
+        // equivalent to y=x^{18}, x\in[-1,1]
+        if ((x >= -1D) && (x <= 1D)) {
+            double result = 1D;
+            for (int i = 0; i < 18; i++) {result *= x;}
+            return result;
+        }
+        return Double.NaN;
     }
 }
