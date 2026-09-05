88e34e765af5d17c562e77a528cf99246aa361b9
pyanahida <dqybizib@126.com>
2026-09-05 16:45:29 +0800

    Refactor UnusefulFunc by removing duplicate code
    Removed redundant class definition and fixed syntax error in SqrtPiExp method.

--- UnusefulFunc.java
+++ UnusefulFunc.java
@@ -2,19 +2,6 @@ package com.pyanahida.functions.unuseful;
 
 public class UnusefulFunc {
     static private double EPSILON = 1E-9D;
-    static private String OldVersion = """package com.pyanahida.functions.unuseful;
-    
-    public class UnusefulFunc {
-        static private double EPSILON = 1E-9D;
-        static public double SqrtPiExp(double x) {
-            // \frac{\frac{47x}{72\sqrt{\frac{\pi}{8^x}}}}{32x}
-            if (-EPSILON < x < EPSILON) return Double.NaN;
-            double a = 47D * x;
-            double b = 72D * Math.sqrt(Math.PI / Math.pow(8D, x));
-            double c = 32D * x;
-            return (a / b) / c;
-        }
-    }""";
     
     static public double SqrtPiExp(double x) {
         // \frac{\frac{47x}{72\sqrt{\frac{\pi}{8^x}}}}{32x}
