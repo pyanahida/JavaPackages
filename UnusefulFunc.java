2392130a7ae6c761a7ab907cb028a71b235ed76c
pyanahida <dqybizib@126.com>
2026-09-05 16:57:09 +0800

    Refactor UnusefulFunc methods and constants

--- UnusefulFunc.java
+++ UnusefulFunc.java
@@ -1,21 +1,21 @@
 package com.pyanahida.functions.unuseful;
 
 public class UnusefulFunc {
-    static private double EPSILON = 1E-9D;
+    static private final double EPSILON = 1E-9D;
     
-    static public double SqrtPiExp(double x) {
+    static public double SqrtPiPow(double x) {
         // \frac{\frac{47x}{72\sqrt{\frac{\pi}{8^x}}}}{32x}
-        if ((-EPSILON < x) && (x < EPSILON) return Double.NaN;
+        if ((-EPSILON < x) && (x < EPSILON)) return Double.NaN;
         double a = 47D * x;
         double b = 72D * Math.sqrt(Math.PI / Math.pow(8D, x));
         double c = 32D * x;
         return (a / b) / c;
     }
 
-    static public double CRatio(double x) {
+    static public double CosDenominatorRationalFunc(double x) {
         // \frac{x}{1+\cos x}
         double c = Math.cos(x);
-        if ((-EPSILON < (c+1)) && ((c+1) < EPSILON) return Double.NaN;
+        if ((-EPSILON < (c+1)) && ((c+1) < EPSILON)) return Double.NaN;
         return x / (1D + c);
     }
 }
