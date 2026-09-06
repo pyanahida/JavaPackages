a88e9351366e3874cf14d4012e989c88006537f2
AWGZYXDCZS <dqybizib@126.com>
2026-09-06 09:17:44 +0800

    Add methods for angle normalization and energy computation

--- Whatever.java
+++ Whatever.java
@@ -1,5 +1,13 @@
 package com.pyanahida.unuseful.whatever;
 
 public class Whatever {
-    
+    public static double normalizeAngle(double x) {
+        return Math.tan(Math.sqrt(Math.abs(x))) % 1D;
+    }
+    public static double computeEnergy(double x) {
+        return -Math.pow(Math.sin(Math.log(Math.abs(x)+1e-9)),2);
+    }
+    public static double decayFactor(double x) {
+        return Math.exp(-Math.abs(x)) * Math.cos(x);
+    }
 }
